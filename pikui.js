e8ebbc213d79648650c19b694126ec31012416ab
Sparklingman <hi@qd.ua>
2022-01-09 16:45:05 +0200

    v0.0.3

--- pikui.js
+++ pikui.js
@@ -1,3 +1,15 @@
+for (let imgC = 0; imgC < 4; imgC++) {
+    let random = Math.floor(Math.random() * (100 - 1 + 1)) + 1
+    let url = 'https://picsum.photos/id/' + random + '/1000'
+    let dataPikui = document.createElement('a')
+    dataPikui.setAttribute('data-pikui', imgC)
+    let src = document.createElement('img')
+    src.setAttribute('src', url)
+    src.setAttribute('alt', 'Random Image #' + random)
+    dataPikui.prepend(src)
+    document.body.prepend(dataPikui)
+}
+
 for (var i = 0; i < document.images.length; i++) {
     let pikui = document.querySelectorAll('a[data-pikui] > img')[i]
 
@@ -16,17 +28,27 @@ for (var i = 0; i < document.images.length; i++) {
         img.setAttribute('src', this.src)
 
         let timer = setInterval(function() {
-            let timePassed = Date.now() - start;
-            img.style.bottom = 0 + 'vmin';
-            img.style.opacity = 1;
-            if (timePassed < 2000) clearInterval(timer);
+            let timePassed = Date.now() - start
+            img.style.transform = 'scale(1)'
+            img.style.opacity = 1
+            if (timePassed < 2000) clearInterval(timer)
         });
         modal.prepend(img)
 
-        document.body.prepend(modal)
+        document.body.append(modal)
 
-        close.onclick = function() {
-            modal.remove()
+        function rm() {
+            img.style.transform = 'scale(.75)'
+            modal.style.opacity = 0
+            modal.style.pointerEvents = 'none'
+            setTimeout(() => modal.remove(), 1000);
         }
+        close.onclick = rm
+
+        window.onkeydown = function(e) {
+            if (e.keyCode == 27) {
+                rm()
+            }
+        };
     }
 }
