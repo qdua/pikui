59d2b3083b129c81bf6224743dcefb8f540c796e
Sparklingman <hi@qd.ua>
2022-01-11 13:10:41 +0200

    v.0.0.5

--- pikui.js
+++ pikui.js
@@ -1,3 +1,4 @@
+//Picsum Photos
 for (let imgC = 0; imgC < 4; imgC++) {
     let random = Math.floor(Math.random() * (100 - 1 + 1)) + 1
     let url = 'https://picsum.photos/id/' + random + '/1000'
@@ -9,13 +10,30 @@ for (let imgC = 0; imgC < 4; imgC++) {
     dataPikui.prepend(src)
     document.body.prepend(dataPikui)
 }
+//
+
+let head = document.getElementsByTagName('head')[0]
+let style = document.createElement('style')
+style.setAttribute('type', 'text/css')
+let rules = {
+    '#pikui': 'background:#202020bb;bottom:0;display:grid;height:100vh;place-items:center;position:fixed;transition: .2s linear;width:100vw;z-index: 100000;',
+    '#pikui s': 'color:#000;cursor:pointer;font: normal 4vmin/1 sans-serif;position:absolute;right:4vw;text-decoration:none;text-shadow: 0 1vmin 2vmin #828282;top:4vh;',
+    '#pikui s::after': 'content: "×";',
+    '#pikui img': 'bottom:1vmin;max-width:80vmin;opacity: 0;transform:scale(.75);transition: .2s linear;',
+    '#pikui span': 'bottom:2vh;position:absolute;text-align:center;'
+};
+head.appendChild(style)
+let sheet = style.sheet
+for (let rule in rules) {
+    sheet.insertRule(`${rule} {${rules[rule]}}`, 0)
+}
 
 for (var i = 0; i < document.images.length; i++) {
     let pikui = document.querySelectorAll('a[data-pikui] > img')[i]
 
     pikui.onclick = function() {
         let modal = document.createElement('div')
-        modal.setAttribute('id', 'modal')
+        modal.setAttribute('id', 'pikui')
 
         let span = document.createElement('span')
         span.innerHTML = pikui.alt
